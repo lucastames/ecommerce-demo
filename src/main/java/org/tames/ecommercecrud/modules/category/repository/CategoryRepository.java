94d0fd717fbaa246247e15cc246eaa59c0e0df02
Lucas Tames <tamess@protonmail.com>
2024-09-13 00:49:22 -0300

    feat: add category repository

--- src/main/java/org/tames/ecommercecrud/modules/category/repository/CategoryRepository.java
+++ src/main/java/org/tames/ecommercecrud/modules/category/repository/CategoryRepository.java
@@ -1,6 +1,8 @@
 package org.tames.ecommercecrud.modules.category.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.tames.ecommercecrud.modules.category.entity.Category;
 
-public interface CategoryRepository extends JpaRepository<Category, Long> {}
+public interface CategoryRepository
+    extends JpaRepository<Category, Long>, JpaSpecificationExecutor<Category> {}
