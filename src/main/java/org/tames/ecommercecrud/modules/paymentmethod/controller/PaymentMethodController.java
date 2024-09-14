47d37f123519d37b9605c53d9e798253265a5e30
Lucas Tames <tamess@protonmail.com>
2024-09-14 15:00:00 +0300

    feat: add payment-method controller

--- src/main/java/org/tames/ecommercecrud/modules/paymentmethod/controller/PaymentMethodController.java
+++ src/main/java/org/tames/ecommercecrud/modules/paymentmethod/controller/PaymentMethodController.java
@@ -1,23 +1,21 @@
 package org.tames.ecommercecrud.modules.paymentmethod.controller;
 
 import jakarta.validation.Valid;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PagedModel;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 import org.tames.ecommercecrud.modules.paymentmethod.dto.PaymentMethodResponseDto;
 import org.tames.ecommercecrud.modules.paymentmethod.dto.SavePaymentMethodRequestDto;
 import org.tames.ecommercecrud.modules.paymentmethod.service.PaymentMethodService;
-import org.tames.ecommercecrud.modules.paymentmethod.specification.PaymentMethodSpecification;
+import org.tames.ecommercecrud.modules.paymentmethod.specification.PaymentMethodSpecs.PaymentMethodFilter;
 
 @RestController
 @RequestMapping("payment-methods")
 public class PaymentMethodController {
   private final PaymentMethodService paymentMethodService;
-  private final Logger log = LogManager.getLogger(PaymentMethodController.class);
 
   public PaymentMethodController(PaymentMethodService paymentMethodService) {
     this.paymentMethodService = paymentMethodService;
@@ -25,17 +23,12 @@ public class PaymentMethodController {
 
   @GetMapping
   public ResponseEntity<PagedModel<PaymentMethodResponseDto>> getPaymentMethods(
-      Pageable pageable, PaymentMethodSpecification specification) {
-    return ResponseEntity.ok(paymentMethodService.getPaymentMethods(pageable, specification));
-  }
-
-  @GetMapping("/{paymentMethodId}")
-  public ResponseEntity<PaymentMethodResponseDto> getPaymentMethod(
-      @PathVariable Long paymentMethodId) {
-    return ResponseEntity.ok(paymentMethodService.getPaymentMethod(paymentMethodId));
+      Pageable pageable, PaymentMethodFilter filter) {
+    return ResponseEntity.ok(paymentMethodService.getPaymentMethods(pageable, filter));
   }
 
   @PostMapping
+  @PreAuthorize("hasRole('ADMIN')")
   public ResponseEntity<PaymentMethodResponseDto> createPaymentMethod(
       @RequestBody @Valid SavePaymentMethodRequestDto savePaymentMethodRequestDto,
       UriComponentsBuilder ucb) {
@@ -47,7 +40,14 @@ public class PaymentMethodController {
         .body(createdPaymentMethod);
   }
 
+  @GetMapping("/{paymentMethodId}")
+  public ResponseEntity<PaymentMethodResponseDto> getPaymentMethod(
+      @PathVariable Long paymentMethodId) {
+    return ResponseEntity.ok(paymentMethodService.getPaymentMethod(paymentMethodId));
+  }
+
   @PutMapping("/{paymentMethodId}")
+  @PreAuthorize("hasRole('ADMIN')")
   public ResponseEntity<PaymentMethodResponseDto> updatePaymentMethod(
       @PathVariable Long paymentMethodId,
       @RequestBody @Valid SavePaymentMethodRequestDto savePaymentMethodRequestDto) {
@@ -56,6 +56,7 @@ public class PaymentMethodController {
   }
 
   @DeleteMapping("/{paymentMethodId}")
+  @PreAuthorize("hasRole('ADMIN')")
   public ResponseEntity<Void> deletePaymentMethod(@PathVariable Long paymentMethodId) {
     paymentMethodService.deletePaymentMethod(paymentMethodId);
     return ResponseEntity.noContent().build();
